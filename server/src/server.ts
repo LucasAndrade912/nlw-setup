410b4c5e0f8a4e6895dbdea492ba7ed08a4d0978
Lucas Andrade <lucasantos.dev@gmail.com>
2023-01-17 22:31:42 -0300

    create routes to create new habit and get day details

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,15 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
-import { PrismaClient } from '@prisma/client'
+
+import { appRoutes } from './routes'
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get('/', () => {
-  return 'Hello World'
-})
+app.register(appRoutes)
 
 app.listen({
-  port: 3000
+  port: 3333
 }).then(() => {
-  console.log('🚀 Server running in localhost:3000')
+  console.log('🚀 Server running in localhost:3333')
 })
